f123a24dbe14b611c7fc332b64f2969a142afa1f
miminu <kios917@gmail.com>
2023-03-10 10:38:36 +0900

    # 임시 단계 저장

---
+++ app/app/src/main/java/com/capston/eduguide/FeedPage.java
@@ -0,0 +1,14 @@
+package com.capston.eduguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_page);
+    }
+}
