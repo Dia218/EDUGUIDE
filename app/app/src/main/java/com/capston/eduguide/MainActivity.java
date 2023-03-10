3c74ea7837f8e0e0e755a8e9a94cde38188d4e28
손지민 <annie1591@kyonggi.ac.kr>
2023-03-10 11:52:38 +0900

    #임시 커밋

---
+++ app/app/src/main/java/com/capston/eduguide/MainActivity.java
@@ -0,0 +1,14 @@
+package com.capston.eduguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
