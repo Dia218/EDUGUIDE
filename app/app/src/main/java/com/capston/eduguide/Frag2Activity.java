1632cc5580dce8ddae9e372504125e024a83c1bf
CyoRRim <tg305@naver.com>
2023-04-07 11:37:44 +0900

    검색화면 구성

--- app/app/src/main/java/com/capston/eduguide/Frag2Activity.java
+++ app/app/src/main/java/com/capston/eduguide/Frag2Activity.java
@@ -1,25 +1,75 @@
 package com.capston.eduguide;
 
+import androidx.fragment.app.Fragment;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import com.example.bottomnavi.R;
-
+import java.util.ArrayList;
 public class Frag2Activity extends Fragment {
 
-    private View view;
+    private RecyclerView recyclerView;
 
-    @Nullable
+    private RecyclerView.LayoutManager layoutManager;
+
+    private SearchAdapter adapter;
+
+    private ArrayList<SearchItem> searchlist;
+
+    private DBHelper dbhelper;
+
+    public Frag2Activity(){
+
+    }
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.frag2, container, false);
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.frag2, container, false);
+
+        recyclerView=view.findViewById(R.id.recycle_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager=new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        dbhelper=new DBHelper(getContext());
+
+        SearchView searchView = view.findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query){
+              //검색어 완료시 호출되는 메소드
+            }
+            @Override
+            public boolean onQueryTextChange(String newText){
+                search(newText);
+                return false;
+            }
+        });
 
         return view;
+    }
+    private void search(String searchText){
+        //SQLite에서 검색어와 일ㄹ치하는 데이터를 찾아서 Arraylist에 추가
+        searchlist = new ArrayList<SearchItem>();
+        SQLiteDatabase db=DBHelper.getWriteableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM mytable WHERE tag='" + searchText + "'", null);
+
+        if(cursor.moveToFirst()){
+            do{
+                String title=cursor.getString(cursor.getColumnIndex("title"));
+                String tag=cursor.getString(cursor.getColumnIndex("tag"));
+                searchlist.add(new SearchItem(title,tag));
+            }while (cursor.movetoNext());
+        }
+        adapter = new SearchAdapter(searchlist);
+        recyclerView.setAdapter(adapter);
     }
 }
